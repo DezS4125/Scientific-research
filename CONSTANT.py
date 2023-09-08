a4d616d345a72fece612c8de41e300f42de0ac06
DezS4125 <workingdezs@gmail.com>
2023-09-08 11:35:26 +0000

    fixed a minor typo

--- CONSTANT.py
+++ CONSTANT.py
@@ -1,5 +1,5 @@
 APP_NAME = "Facial Analytic"
 KAFKA_TOPIC_INPUT = "input-1"
-BOOTSTRAP_SERVERS = ['worker1:9091','worker2:9093','worker3:9093']
+BOOTSTRAP_SERVERS = ['worker1:9091','worker2:9093','worker3:9094']
 SPARK_MASTER = "spark://192.168.81.106:7077"
 KAFKA_TOPIC_OUTPUT = "result-1"
