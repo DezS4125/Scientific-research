cac9b93462c8b57cb7736e6cc0ef128e2866bb01
dezs <workingdezs@gmail.com>
2023-09-08 19:45:18 +0700

    new sparkApp, not sure if it work

--- sparkApp.py
+++ sparkApp.py
@@ -2,16 +2,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import cv2
 import numpy as np
-from io import BytesIO
 from kafka import KafkaProducer
-
 from CONSTANT import APP_NAME,KAFKA_TOPIC_INPUT,BOOTSTRAP_SERVERS,KAFKA_TOPIC_OUTPUT
 
 spark = SparkSession \
     .builder \
     .appName(APP_NAME) \
     .getOrCreate()
-
+    
 # Subscribe to 1 topic
 df = spark \
     .readStream \
@@ -21,41 +19,31 @@ df = spark \
     .load()
 df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
 
-print("hello 1")
-def process(row):
-    image = row["value"]
-    filename = f"/home/ubuntu/codes/kafka/prod/output/image-{row['key']}.png"
-    print("hello 2")
+producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS)
 
-    # Convert the image data to a NumPy array
-    nparr = np.frombuffer(image, np.uint8)
-    
-    # Decode the image data using OpenCV
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    
-    # Display the frame in the window
-    print("image received")
-    
-    # Serialize the image data
-    img_bytes = BytesIO()
-    np.save(img_bytes, image)
-    
-    # Publish the image data to a Kafka topic
-    try:
-        producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS)
-        producer.send(KAFKA_TOPIC_OUTPUT, key=row['key'], value=img_bytes.getvalue())
-        producer.flush()
-        print("image sent to kafka topic")
-    except Exception as e:
-        print(f"Error sending message to Kafka: {e}")
-    finally:
-        producer.close()
+def process(batchDF, batchId):
+    images = batchDF.select("value").rdd.map(lambda x: x["value"]).collect()
+    for i, image in enumerate(images):
+        # Convert the image data to a NumPy array
+        nparr = np.frombuffer(image, np.uint8)
+        
+        # Decode the image data using OpenCV
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        
+        # Process the image as needed
+        # ...
+        
+        # Encode the processed image as a JPEG
+        ret, buf = cv2.imencode(".jpg", image)
+        image_bytes = buf.tobytes()
+        
+        # Write the processed image to a Kafka topic
+        producer.send(KAFKA_TOPIC_OUTPUT, value=image_bytes)
 
-# Start the streaming query
 query = df \
     .writeStream \
     .outputMode("append") \
-    .foreach(process) \
+    .foreachBatch(process) \
     .start()
 
 query.awaitTermination()
